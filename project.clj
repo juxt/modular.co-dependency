8f320dccee3e8c51cdfbc1a31224319481c88bc4
Malcolm Sparks <malcolm@juxt.pro>
2015-06-29 00:46:48 +0100

    Add schema helper

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject juxt.modular/co-dependency "0.2.0"
+(defproject juxt.modular/co-dependency "0.2.1"
   :description "Co-dependency support for com.stuartsierra.component"
   :url "https://github.com/juxt/modular.co-dependency"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.stuartsierra/component "0.2.2"]])
+  :dependencies [[com.stuartsierra/component "0.2.3"]
+                 [prismatic/schema "0.4.3"]])
