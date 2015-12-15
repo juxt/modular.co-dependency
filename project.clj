35ebd3aa8a0233361881511e62711e7d6dbf7074
Malcolm Sparks <malcolm@juxt.pro>
2015-12-15 18:30:55 +0000

    Update to schema 1.0.3

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject juxt.modular/co-dependency "0.2.2"
+(defproject juxt.modular/co-dependency "0.3.0"
   :description "Co-dependency support for com.stuartsierra.component"
   :url "https://github.com/juxt/modular.co-dependency"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[com.stuartsierra/component "0.2.3"]
-                 [prismatic/schema "0.4.3"]])
+  :dependencies [[com.stuartsierra/component "0.3.1"]
+                 [prismatic/schema "1.0.3"]])
--- src/modular/component/co_dependency/schema.clj
+++ src/modular/component/co_dependency/schema.clj
@@ -2,19 +2,22 @@
 (ns modular.component.co-dependency.schema
   (:require
    schema.utils schema.macros
-   [schema.core :as s]))
+   [schema.core :as s]
+   [schema.spec.core :as spec]
+   [schema.spec.collection :as collection]))
+
+(defn deref? [x]
+  (isa? (type x) clojure.lang.IDeref))
 
 (defrecord Co-dependency [schema]
   s/Schema
-  (s/walker [this]
-    (let [sub-walker (s/subschema-walker schema)]
-      (fn [x]
-        (if x
-          (let [res (sub-walker @x)]
-            (when (schema.utils/error? res) res))
-          (schema.macros/validation-error this x (list 'injected? schema (schema.utils/value-name x)))))))
-
-  (s/explain [this] this))
+  (spec [this]
+    (collection/collection-spec
+     (spec/simple-precondition this deref?)
+     promise
+     [(collection/one-element true schema (fn [item-fn coll] (item-fn @coll) nil))]
+     (clojure.core/fn [_ xs _] (let [p (promise)] (deliver p (first xs))))))
+  (explain [this] (list 'co-dep (s/explain schema))))
 
 (defn co-dep [schema]
   (->Co-dependency schema))
